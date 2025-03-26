d5cc8b16ef694aa6703596a20c2821e999137516
Scythe6699 <nabilm2001@gmail.com>
2025-03-26 17:02:03 +0000

    Added year validations

--- src/validate_birthday.py
+++ src/validate_birthday.py
@@ -1,2 +1,23 @@
 def validate_birthday(birthday):
-    pass
+    birthday_list = str(birthday).split()
+
+    # handle error when input not in the correct format
+    if len(birthday_list) < 3:
+        raise ValueError("Invalid Input!\nError: Incorrect Format!")
+
+    # get the data
+    day = birthday_list[0]
+    month = birthday_list[1]
+    year = birthday_list[0]
+
+    # check day and year
+    if (day.isdigit() == False) or (year.isdigit() == False):
+        raise ValueError("Invalid Input!\nError: Invalid Day or Year!")
+
+    # check year
+    if 0 > year > 2025:
+        raise ValueError("Invalid year!\nError: Out of range")
+
+
+    
+
