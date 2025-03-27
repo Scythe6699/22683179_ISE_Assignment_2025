a5b40810b8f239fbdea66323e8512593b0af3eec
Scythe6699 <nabilm2001@gmail.com>
2025-03-27 09:49:18 +0000

    added luck color function

---
+++ src/lucky_color.py
@@ -0,0 +1,18 @@
+def lucky_color(lpn):
+    # lucky color dictionary
+    color = {
+        1: "Red",
+        2: "Orange",
+        3: "Yellow",
+        4: "Green",
+        5: "Sky Blue",
+        6: "Indigo",
+        7: "Violet",
+        8: "Magenta",
+        9: "Gold",
+        11: "Silver",
+        22: "White",
+        33: "Crimson"
+    }
+
+    return color[lpn]
