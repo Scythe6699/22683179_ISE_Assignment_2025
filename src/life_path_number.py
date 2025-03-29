952859ada2382951b2f67daa978c6258a77e0bf3
Scythe6699 <nabilm2001@gmail.com>
2025-03-29 14:21:34 +0000

    Refactor life_path_number function to improve clarity and efficiency in master number handling

--- src/life_path_number.py
+++ src/life_path_number.py
@@ -1,7 +1,7 @@
 def life_path_number(day, month, year):
     # master numbers
     master_num = [11, 22, 33]
-
+    
     # add digits
     def add_digit(data):
         total = 0
@@ -14,28 +14,23 @@ def life_path_number(day, month, year):
     def greater_formatter(num):
         if num <= 9:
             return num
+        elif num in master_num:
+            return num
         return greater_formatter(add_digit(num))
-
+    
     # life path check
-
-    ## check if any of them are masters
-    if day not in master_num:
-        day = greater_formatter(day)
-
-    if month not in master_num:
-        month = greater_formatter(month)
-
-    if year not in master_num:
-        year = greater_formatter(year)
-
+    ## get the individual sums
+    day_sum = day if day in master_num else greater_formatter(day)
+    month_sum = month if month in master_num else greater_formatter(month)
+    year_sum = year if year in master_num else greater_formatter(year)
+    
     ## total
-    total = day + month + year
-
-    ## check if total is master
+    total = day_sum + month_sum + year_sum
+    
+    ## check if total is a master number or needs further reduction
     if total in master_num:
         return total
-
-    ## sum of total digits
-    output = add_digit(total)
+    elif total > 9:
+        return greater_formatter(total)
     
-    return output
+    return total
